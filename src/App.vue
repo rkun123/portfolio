4262d60018d2d2b5bb1d7a0164ab7e4979938f17
rkun123 <tren111213@gmail.com>
2020-10-01 01:23:27 +0900

    [add] Apply 'Google Noto Sans JP' fonts

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,7 @@ body {
   margin: 0;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Noto Sans JP', Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    css: {
+        loaderOptions: {
+            scss: {
+                additionalData: "@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;700&display=swap');"
+            }
+        }
+    }
+}
